447f6281e3d489b00561e86725e543713da1dd52
imojidra <150125598+imojidra@users.noreply.github.com>
2024-06-05 12:17:47 +0530

    Add files via upload

---
+++ tnsif/newpackage/personalInfo/DemoPerson.java
@@ -0,0 +1,22 @@
+package tnsif.newpackage.personalInfo;
+
+import Day5.CalculateTaxCountry;
+import tnsif.newpackage.countryInfo.*;
+//import tnsif.newpackage.personalInfo.*;
+
+public class DemoPerson {
+	public static void main(String args[])
+	{
+		Country c = new Country("India");
+		State s = new State("Gujarat");
+		Person p = new Person("ishita",c,s,25,90000);
+		CalculateTaxCountry ct = new CalculateTaxCountry();
+		
+		ct.taxCal(p);
+		
+	
+		System.out.println(p.toString());
+		
+		
+	}
+}
