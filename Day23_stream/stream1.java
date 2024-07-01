ec46def47b91cd47c948b0a4660d6454d1c1c05c
Ishita Mojidra <150125598+imojidra@users.noreply.github.com>
2024-07-01 22:21:55 +0530

    Add files via upload

---
+++ Day23_stream/stream1.java
@@ -0,0 +1,22 @@
+package Day23_stream;
+
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class stream1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Stream <Integer> ob = Stream.of(10,20,30,40,50);
+		System.out.println("Count : "+ob.count());
+		
+		Integer[] ob1 = {10,20,30};
+		Stream sint = Arrays.stream(ob1);
+		
+		sint = Arrays.stream(ob1).map(k->k*k);
+		sint.forEach(System.out::println);
+		
+	}
+
+}
